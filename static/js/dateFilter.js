d9e0bf2605431fd186619007a2894e3a8efee008
perossing <perossing@gmail.com>
2018-10-23 17:50:07 -0700

    basic filter js, list pages restructured to target filter groupings

--- static/js/dateFilter.js
+++ static/js/dateFilter.js
@@ -1,10 +1,31 @@
-
+/* jshint esversion: 6 */
 
 // with button or selection, target elements by class...
 
-let Aug = document.querySelectorAll(".August2018");
+let filterItem = document.getElementsByClassName("filter-item");
 
+// let Aug = document.querySelectorAll(".August2018");
 
 // loop or get all...
 // with button or selection, set display to none...
-Aug[0].style.display = "none"; 
+// Aug[0].style.display = "none";
+
+// for (let i = 0; i < Aug.length; i++) {
+//     Aug[i].style.display = "none";
+// }
+
+
+function useFilter(month) { 
+
+    for (let i = 0; i < filterItem.length; i++) {
+
+        if (filterItem[i].classList.contains(month)) {
+            filterItem[i].style.display = "block";
+        } else {
+            filterItem[i].style.display = "none";
+        }
+    }
+}
+
+
+console.log("=== date filter ===");
