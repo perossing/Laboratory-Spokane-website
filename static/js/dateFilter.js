82f0d7ef369a75e506af1ebce45b187ea06690e1
perossing <perossing@gmail.com>
2018-10-25 10:23:20 -0700

    date filter functional for artist page

--- static/js/dateFilter.js
+++ static/js/dateFilter.js
@@ -1,33 +1,87 @@
 /* jshint esversion: 6 */
 
-// with button or selection, target elements by class...
+// (function filterDates() {
+
+    console.log("=== date filter ===");
+
+    let filterItem = document.getElementsByClassName("filter-item");
+    let yearSelect = document.getElementsByClassName("year-select");
+    let dateSelect = document.getElementsByClassName("date-selector");
+    let clearBtn = document.getElementById("clear-button");
+    let resultStatus = document.getElementById("results-status");
+    let result = false;
+    let clickedList;
+
+    for (let i = 0; i < yearSelect.length; i++) {
+        yearSelect[i].onmouseover = (() => {
+            if (clickedList) {
+                clickedList.style.display = "block";
+            }
+        });
+    }
 
 
-console.log("=== date filter ===");
+    for (let i = 0; i < dateSelect.length; i++) {
+        dateSelectClick(i);
+    }
 
-let filterItem = document.getElementsByClassName("filter-item");
-let dateSelect = document.getElementsByClassName("date-selector");
+    function dateSelectClick(i) {
+        dateSelect[i].onclick = (() => {
+            result = false;
+            clickedList = dateSelect[i].parentElement;
+            clickedList.style.display = "none";
+            checkResults(dateSelect[i].id);
+            useFilter(dateSelect[i].id);
+        });
+    }
 
-
-// let Aug = document.querySelectorAll(".August2018");
-
-// loop or get all...
-// with button or selection, set display to none...
-// Aug[0].style.display = "none";
-
-// for (let i = 0; i < Aug.length; i++) {
-//     Aug[i].style.display = "none";
-// }
-
-
-function useFilter(month) { 
-
-    for (let i = 0; i < filterItem.length; i++) {
-
-        if (filterItem[i].classList.contains(month)) {
-            filterItem[i].style.display = "block";
-        } else {
-            filterItem[i].style.display = "none";
+    function checkResults(season) {
+        for (let i = 0; i < filterItem.length; i++) {
+            if (filterItem[i].classList.contains(season)) {            
+                result = true;
+                return;
+            }
         }
     }
-}
+
+    function useFilter(season) {
+        for (let i = 0; i < filterItem.length; i++) {
+            filterItem[i].style.opacity = 0;
+            if (filterItem[i].classList.contains(season)) {
+                changeDisplay(i, 'block');
+                changeOpacity(i);
+                resultStatus.style.display = "none";
+            } else {
+                if (result === false) {
+                    resultStatus.style.display = "block";
+                }
+                changeDisplay(i, 'none');
+            }
+        }
+    }
+
+    clearBtn.onclick = (() => {
+        for (let i = 0; i < filterItem.length; i++) {
+            filterItem[i].style.opacity = 0;
+            changeDisplay(i, 'block');
+            changeOpacity(i);
+            result = false;
+            resultStatus.style.display = "none";
+            console.log("=== CLEARED ===");
+        }
+    });
+
+
+    function changeOpacity(i) {
+        setTimeout(() => {
+            filterItem[i].style.opacity = 1;
+        }, 100);
+    }
+
+    function changeDisplay(i, value) {
+        setTimeout(() => {
+            filterItem[i].style.display = value;
+        }, 100);
+    }
+
+// })();
