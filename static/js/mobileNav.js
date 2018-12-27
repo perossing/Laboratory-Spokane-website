a47a5cc17df340b41b2deb118b5d73700f85f139
perossing <perossing@gmail.com>
2018-12-26 21:06:29 -0800

    color change - mobile nav

--- static/js/mobileNav.js
+++ static/js/mobileNav.js
@@ -11,12 +11,14 @@ navReveal.onclick = (() => {
     if (!menuVisible) {
         mobileNav.style.display = "block";
         mobileNav.style.opacity = "1";
-        navReveal.style.color = '#ea5';
+        navReveal.style.background = '#ea5';
+        // navReveal.style.color = '#ea5';
         navReveal.style.textShadow = '1px 1px 5px #fff';
         menuVisible = true;
     }
     else {
         mobileNav.style.display = "none";
+        navReveal.style.background = '#eee';
         navReveal.style.color = '#222';
         navReveal.style.textShadow = 'none';
         menuVisible = false;
