2aaff546cba7af46d8cfd3189de516e98b64cfaf
perossing <perossing@gmail.com>
2019-01-03 16:00:45 -0800

    new pics in image slider, script moved to file

---
+++ static/js/imageCarousel.js
@@ -0,0 +1,40 @@
+let slideIndex = 1;
+// showDivs(slideIndex);
+
+
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+    showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+    let i;
+    let pics = document.getElementsByClassName("slide-pic");
+    let dots = document.getElementsByClassName("dot");
+    if (n > pics.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = pics.length;
+    }
+    for (i = 0; i < pics.length; i++) {
+        pics[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("dot-current", "");
+    }
+
+    // pics[slideIndex - 1].style.transform = 'translateX(90%)';
+    pics[slideIndex - 1].style.display = "block";
+
+    dots[slideIndex - 1].className += " dot-current";
+}
+
+
+
+
+
